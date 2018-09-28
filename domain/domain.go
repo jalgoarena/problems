cdf4812eac2e3496690a49a66fbecd3e03101c37
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-09-28 18:18:55 +0200

    no need to expose types ...

--- domain/domain.go
+++ domain/domain.go
@@ -1,25 +1,25 @@
 package domain
 
-type ReturnStatement struct {
+type returnStatement struct {
 	Type    string `json:"type"`
 	Comment string `json:"comment"`
 	Generic string `json:"generic"`
 }
 
-type Parameter struct {
+type parameter struct {
 	Name    string `json:"name"`
 	Type    string `json:"type"`
 	Comment string `json:"comment"`
 	Generic string `json:"generic"`
 }
 
-type Function struct {
+type function struct {
 	Name       string          `json:"name"`
-	Return     ReturnStatement `json:"returnStatement"`
-	Parameters []Parameter     `json:"parameters"`
+	Return     returnStatement `json:"returnStatement"`
+	Parameters []parameter     `json:"parameters"`
 }
 
-type TestCase struct {
+type testCase struct {
 	Input  []interface{} `json:"input"`
 	Output interface{}   `json:"output"`
 }
@@ -29,7 +29,7 @@ type Problem struct {
 	Title       string     `json:"title"`
 	Description string     `json:"description"`
 	TimeLimit   int64      `json:"timeLimit"`
-	Function    Function   `json:"func"`
-	TestCases   []TestCase `json:"testCases"`
+	Function    function   `json:"func"`
+	TestCases   []testCase `json:"testCases"`
 	Level       int32      `json:"level"`
 }
