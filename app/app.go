63364e037cd62992e7f84d01e0ada2e67925389e
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-10-01 20:59:03 +0200

    - add health endpoint

--- app/app.go
+++ app/app.go
@@ -5,20 +5,30 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jalgoarena/problems-store/domain"
 	"io"
+	"net/http"
 )
 
 var problems []domain.Problem
 
+func HealthCheck(c *gin.Context) {
+	if problems == nil || len(problems) == 0 {
+		c.JSON(http.StatusServiceUnavailable, gin.H{"status": "fail", "reason": "problems setup failed"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "problemsCount": len(problems)})
+}
+
 // curl -i http://localhost:8080/api/v1/problems
 func GetProblems(c *gin.Context) {
-	c.JSON(200, problems)
+	c.JSON(http.StatusOK, problems)
 }
 
 // curl -i http://localhost:8080/api/v1/problems/fib
 func GetProblem(c *gin.Context) {
 	id := c.Param("id")
 
-	c.JSON(200, filter(problems, func(problem domain.Problem) bool {
+	c.JSON(http.StatusOK, filter(problems, func(problem domain.Problem) bool {
 		return problem.Id == id
 	}))
 }
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
+	router.GET("/health", app.HealthCheck)
 	v1 := router.Group("api/v1")
 	{
 		v1.GET("/problems", app.GetProblems)
