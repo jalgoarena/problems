9404b048e4b10e32d71c7be74d248a73e58f7d6f
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-10-06 19:21:06 +0200

    - return ErrEmpty if based on given problem id problem is not found

--- problm/service.go
+++ problm/service.go
@@ -19,21 +19,23 @@ type problemsService struct{}
 
 func (problemsService) FindById(_ context.Context, problemId string) (*pb.Problem, error) {
 
-	problem := first(problems, func(problem *pb.Problem) bool {
+	problem, err := first(problems, func(problem *pb.Problem) bool {
 		return problem.Id == problemId
 	})
 
-	return problem, nil
+	return problem, err
 }
 
-func first(problems []*pb.Problem, f func(problem *pb.Problem) bool) *pb.Problem {
+var ErrEmpty = errors.New("not found")
+
+func first(problems []*pb.Problem, f func(problem *pb.Problem) bool) (*pb.Problem, error) {
 	for _, problem := range problems {
 		if f(problem) {
-			return problem
+			return problem, nil
 		}
 	}
 
-	return &pb.Problem{}
+	return nil, ErrEmpty
 }
 
 func (problemsService) FindAll(_ context.Context) (*string, error) {
